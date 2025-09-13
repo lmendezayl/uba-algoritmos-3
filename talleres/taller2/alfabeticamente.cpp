7656320ee29738e08240a0d1867119be3b165f3c
lmendezayl <lmendezayl@gmail.com>
2025-09-13 19:58:48 -0300

    arranque taller 2, terminar archivo alfabeticamente.cpp

--- talleres/taller2/alfabeticamente.cpp
+++ talleres/taller2/alfabeticamente.cpp
@@ -1,30 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector> 
 using namespace std;
-typedef long long ll;
 
-int solve(vector<ll> c, vector<string>){
-    
-    return -1;
+
+int solve(vector<int> c, vector<string> s){
+    int res = -1;
+    for (int i = 0; i < s.lenght(); i++){ 
+        int comparar(s[i], s[i+1]);
+    }
+    return res;
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    
     int n; 
     cin >> n;
-    int i = 0;
-    vector<ll> c(n);
-    vector<string> s(n);
-    while(i < n){
-        cin >> c[i];
-        i++;
-    }
-    i = 0;
-    while(i < n){
-        cin >> s[i];
-        i++;
-    }
 
-    int ans = solve(c, s);
+    vector<int> c(n);
+    vector<string> s(n);
+    
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
+    for (int i = 0; i < n; i++)
+        cin >> s[i];
+    
+    solve(c, s);
+    
     return 0;
 }
