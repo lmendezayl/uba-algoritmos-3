3ca9dd948805fa93ae0659f548e939c191c81c9a
Lautaro Mendez <81045740+lmendezayl@users.noreply.github.com>
2025-09-14 20:23:49 -0300

    Update alfabeticamente.cpp

--- talleres/taller2/alfabeticamente.cpp
+++ talleres/taller2/alfabeticamente.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
-#include <string>
 #include <vector> 
+#include <string>
+#include <limits>
+#include <algorithm>
 using namespace std;
 
-
-int solve(vector<int> c, vector<string> s){
-    int res = -1;
-    for (int i = 0; i < s.lenght(); i++){ 
-        int comparar(s[i], s[i+1]);
-    }
-    return res;
-}
-
-int main(){
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int n; 
-    cin >> n;
+    cin.tie(nullptr);
 
-    vector<int> c(n);
-    vector<string> s(n);
-    
-    for (int i = 0; i < n; i++)
+    int n;
+    cin >> n;
+    vector<long long> c(n);
+    vector<string> s(n), rev(n);
+
+    for (int i = 0; i < n; i++){
         cin >> c[i];
-    for (int i = 0; i < n; i++)
+    }
+    for (int i = 0; i < n; i++){
         cin >> s[i];
+        rev[i] = s[i];
+        reverse(rev[i].begin(), rev[i].end());
+    }
+    // dp vector 2x2 de infs
+    long long inf = 1e18;
+
+    vector<vector<long long>> dp(2, vector<long long>(2, inf)); 
+    dp[0][0] = 0;
+    dp[0][1] = c[0];
+
+    for (int i=1; i<n; i++){
+      // no invierto dp[1][0]
+      if (s[i] >= s[i-1]) dp[1][0] = min(dp[1][0], dp[0][0]);
+      if (s[i] >= rev[i-1]) dp[1][0] = min(dp[1][0], dp[0][1]);
+      // invierto (dp[1][1])
+      if (rev[i] >= s[i-1]) dp[1][1] = min(dp[1][1], dp[0][0] + c[i]);
+      if (rev[i] >= rev[i-1]) dp[1][1] = min(dp[1][1], dp[0][1] + c[i]);
+      
+      dp[0][0] = dp[1][0];
+      dp[0][1] = dp[1][1];
+      
+      if (dp[0][0] == inf && dp[0][1] == inf){
+        cout << -1 << endl;
+        return 0;
+      }  
+      
+      dp[1][0] = dp[1][1] = inf;
+    }
     
-    solve(c, s);
-    
+    cout << min(dp[0][0], dp[0][1]);
     return 0;
 }
